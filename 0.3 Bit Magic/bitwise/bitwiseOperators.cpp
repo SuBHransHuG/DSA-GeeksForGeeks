1f6ecbe53ef0b191ff2e6500f86e296441ed203a
SuBHransHuG <69098939+SuBHransHuG@users.noreply.github.com>
2024-04-25 00:52:27 +0530

    first commit

---
+++ 0.3 Bit Magic/bitwise/bitwiseOperators.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int x = 3;
+int y = 6;
+cout<<"Value of Bitwise & is "<<(x&y)<<endl;
+cout<<"Value of Bitwise | is "<<(x|y)<<endl;
+cout<<"Value of Bitwise ^ is "<<(x^y)<<endl;
+return 0;
+}
