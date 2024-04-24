233c9dc0531183dbbcb10cb94712bd1af8b79368
SuBHransHuG <69098939+SuBHransHuG@users.noreply.github.com>
2024-04-25 01:40:08 +0530

    bitwiseNot

---
+++ 0.3 Bit Magic/bitwise/bitwiseNot.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // unsigned
+unsigned int x = 2;
+cout<<"Negation of "<<x<<" is "<<(~x)<<endl;
+x = 5;
+cout<<"Negation of "<<x<<" is "<<(~x)<<endl;
+// signed
+int y = 2;
+cout<<"Negation of "<<y<<" is "<<(~y)<<endl;
+y = 5;
+cout<<"Negation of "<<y<<" is "<<(~y)<<endl;
+return 0;
+}
