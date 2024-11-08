63da684660320a7b33756621e9eb1a587514d4df
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2024-11-08 22:03:40 +0530

    Create test6.java
    Output-1101
    Working on the principle of binary equivalent.

---
+++ 04. Recursion/test6.java
@@ -0,0 +1,11 @@
+public class Test6 {
+    static void fun(int n){
+        if(n==0)
+         return;
+        fun(n/2);
+        System.out.println(n%2);
+    }
+    public static void main(String[] args) {
+        fun(13);
+    }
+}
