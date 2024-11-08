8bb790706cf881866d63092deca339e7ff7b2a87
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2024-11-08 19:24:04 +0530

    Output Practice 1
    Output
    -Code1
    3
    2
    1
    1
    2
    3
    -code2
    1
    2
    1
    3
    1
    2
    1

--- 04. Recursion/test2.java
+++ 04. Recursion/test2.java
@@ -1,3 +1,4 @@
+//Code1
 public class Test2 {
     static void func1(int n){
         if(n==0)
@@ -10,3 +11,19 @@ public class Test2 {
         func1(3);
     }
 }
+
+//Code2
+public class Test3 {
+    static void func1(int n)
+    {
+        if(n==0)
+            return;
+        func1(n-1);
+        System.out.println(n);
+        func1(n-1);
+    }
+    public static void main(String[] args) {
+        func1(3);
+    }
+}
+
