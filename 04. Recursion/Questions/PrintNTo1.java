2e40e706c4a9e51e090b1bc09a76a18e9dfb0e54
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2024-11-08 22:04:46 +0530

    Print N To 1

---
+++ 04. Recursion/Questions/PrintNTo1.java
@@ -0,0 +1,11 @@
+public class PrintNTo1 {
+    static void fun(int n){
+        if(n==0)
+            return;
+        System.out.println(n);
+        fun(n-1);
+    }
+    public static void main(String[] args) {
+        fun(5);
+    }
+}
